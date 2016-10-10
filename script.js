846f459ea58c0bb949172746679e83e1108fb6ca
Yvonne Chen <yc.yvonnechen@yahoo.com>
2016-10-10 03:02:56 -0700

    Updated home page

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {
-  // Your Javascript code here
+$(document).ready(function(){
+  $('.wrapper').mousemove(function(e){
+  	//5,10,15,20,25
+
+  	$(".title_wrapper").animate({"left": "100px"}, 1500);
+
+    var dict = [];
+
+    dict.push(
+    {
+        key:   ".parallax",
+        value: 10
+    },
+    {   
+        key:   "#snow1",
+        value: 5
+    },
+    {
+        key:   ".ball",
+        value: 50
+    });
+
+    console.log(dict[0].key);
+
+    for(var i = 0; i < 3; i++){
+      var x = -(e.pageX + this.offsetLeft) / dict[i].value;
+      var y = -(e.pageY + this.offsetTop) / dict[i].value;
+      $(dict[i].key).css('margin', x + 'px ' + y + 'px');
+    }
+    
+  });    
 });
