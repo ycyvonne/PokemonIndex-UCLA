224eafac16aa19d8be411438273372c806901f3f
Yvonne Chen <yc.yvonnechen@yahoo.com>
2016-10-10 20:50:32 -0700

    additional edits

--- script.js
+++ script.js
@@ -2,29 +2,34 @@ $(document).ready(function(){
   $('.wrapper').mousemove(function(e){
   	//5,10,15,20,25
 
-  	$(".title_wrapper").animate({"left": "100px"}, 1500);
+  	$(".title_wrapper").animate({"top": "150px", "opacity" : "1"}, 800);
 
     var dict = [];
 
-    dict.push(
-    {
-        key:   ".parallax",
-        value: 10
-    },
-    {   
-        key:   "#snow1",
-        value: 5
-    },
-    {
-        key:   ".ball",
-        value: 50
+    dict.push({
+      key:   ".ball",
+      value: 50
     });
 
-    console.log(dict[0].key);
+    var snow = "#snow";
+    var degree = 50;
+    for (var i = 1; i < 6; i++){
+      dict.push({key: snow + i, value: degree});
 
-    for(var i = 0; i < 3; i++){
+      degree -= 10;
+      if (degree <= 0)
+        degree = 50;
+    }
+
+    for(var i = 0; i < 6; i++){
       var x = -(e.pageX + this.offsetLeft) / dict[i].value;
       var y = -(e.pageY + this.offsetTop) / dict[i].value;
+
+      if (i%2 == 0){
+        var temp = x;
+        x = y;
+        y = temp;
+      }
       $(dict[i].key).css('margin', x + 'px ' + y + 'px');
     }
     
